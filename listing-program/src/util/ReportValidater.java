69a1f51527a03c3333b752a7e508b97231ccdb10
Evil Tree FC <eviltreefc@outlook.com>
2017-01-17 21:37:12 +0100

    Added utility package and class
    Package util will contain utility classes that can perform various
    computations to lessen the workload of the panels in package gui.
    Furthermore it sections off the logical part from the gui, which makes
    more sense.

---
+++ listing-program/src/util/ReportValidater.java
@@ -0,0 +1,16 @@
+package util;
+
+public final class ReportValidater {
+	/**
+	 * Constructor declared {@code private} in order to suppress the default
+	 * constructor, ensuring that {@code ReportValidater} cannot be
+	 * instantiated.
+	 */
+	private ReportValidater() {
+		
+	} // end constructor()
+	
+	public static boolean isDataValid( String world, String minutes ) {
+		return false;
+	} // end method isReportValid():boolean
+} // end class ReportValidater
